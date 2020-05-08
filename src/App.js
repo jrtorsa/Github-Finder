4fe5ba8115106453644f042d0af7f0d50576816c
Raul T <jr.torsa@gmail.com>
2020-05-08 16:17:51 -0600

    Adding props to nav bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, {
   Component
 } from 'react';
+
+import Navbar from './components/layout/Navbar'
 import './App.css';
 
-class App extends React.Component {
+class App extends Component {
   render() {
     return ( 
       <div className = "App" >
-        <h1> Hello from React </h1>  
+        <Navbar /> 
       </div>
     );
   }
